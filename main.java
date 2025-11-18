7ff0eef356858e28c806c70ddae26f6121ebfb77
Ayman21dg <160420116+Ayman21dg@users.noreply.github.com>
2025-11-18 16:15:03 +0100

    test a change

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test hello!");
-        System.out.println("bonjour hajar");
+        System.out.println("Test test!");
+        System.out.println("bonjour prof");
     }
 }
